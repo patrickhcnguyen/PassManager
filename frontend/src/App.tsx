7fea217263bde1a63112184fdbc4c6e7be69f932
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-25 15:35:42 -0700

    tanstack query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './components/Register/register'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
