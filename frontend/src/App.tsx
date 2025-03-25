6736376095922fd00197a94ac84f537ebd8001b1
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-24 19:28:00 -0700

    init frontend with react, vite, and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 min-h-screen flex items-center justify-center font-bold text-4xl">Hello People</h1>
+    </div>
+  )
+}
+
+export default App
