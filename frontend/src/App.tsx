bc390b539ae502a7ef32435b8b93f78e33b1f5af
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-25 19:21:26 -0700

    login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './components/Register/register'
+import Login from './components/Login/login'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </QueryClientProvider>
