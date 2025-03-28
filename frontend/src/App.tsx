06ed53baa383062c87f73547508025ffc852c743
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-27 22:30:10 -0700

    dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './components/Register/register'
 import Login from './components/Login/login'
+import Dashboard from './components/Dashboard/dashboard'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </QueryClientProvider>
